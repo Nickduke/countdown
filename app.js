ce1badd9c11806fdffeffe564eb633f773c3c3d5
Nick <ntoukas.nick@gmail.com>
2022-11-20 15:39:53 -0500

    countdown timer complete

--- app.js
+++ app.js
@@ -41,3 +41,40 @@ let day = futureDate.getDay();
 day = weekdays[day];
 
 giveaway.textContent = `giveaway ends on ${day}, ${month} ${date}, ${year}, ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  const format = (item) => {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+
+    return item;
+  };
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry this giveaway has expired</h4>`;
+  }
+};
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
